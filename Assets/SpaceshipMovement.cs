c50864fc3980c01a42309f971c9316a87ce26e80
Alex Batrinu <alexb@giffgaff.co.uk>
2017-08-07 16:20:04 +0100

    Initial master commit

---
+++ Assets/SpaceshipMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour {
+
+	public float maxPos = 2.43f;
+	public float shipSpeed;
+
+	Vector3 position;
+	Rigidbody2D rb;
+
+	void Awake(){
+
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Start(){
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		AccelerometerMove ();
+
+		position = transform.position;
+		position.x = Mathf.Clamp (position.x, -2.43f, 2.43f);
+		transform.position = position;
+	}
+
+	void AccelerometerMove(){
+
+		float x = Input.acceleration.x;
+		Debug.Log ("X = " + x);
+
+
+		if (x < -0.1f) {
+			MoveLeft ();
+		} else if (x > 0.1f) {
+			MoveRight ();	
+		} 
+		else {
+			SetVelocityZero();
+		}
+
+	}
+
+	public void MoveLeft(){
+		rb.velocity = new Vector2 (-shipSpeed, 0);
+	}
+
+	public void MoveRight(){
+
+		rb.velocity = new Vector2 (shipSpeed, 0);
+	}
+
+	public void SetVelocityZero(){
+		rb.velocity = Vector2.zero;
+	}
+}
